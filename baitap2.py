32fa1bc80af82e80c6283a5e03e235e67ce74144
Nyakkon <Kyuubi.yae@wibu.me>
2023-07-10 21:35:30 +0700

    first commit

---
+++ baitap2.py
@@ -0,0 +1,1 @@
+#In hello world ra màn hình
---
+++ baitap3.py
@@ -0,0 +1,15 @@
+#sử dụng if else
+# Viết chương trình Python tính giá trị trung bình (avg) của ba biến a, b, c nhập từ bàn phím (a, b, c là ba số tự nhiên) với điều kiện như sau:
+
+# Nếu avg > a và avg > b thì hiển thị The average value is greater than both a and b
+# Nếu avg > a và avg > c thì hiển thị The average value is greater than both a and c
+# Nếu avg > b và avg > c thì hiển thị The average value is greater than both b and c
+# Nếu avg chỉ lớn hơn a thì hiển thị The average value is greater than a
+# Nếu avg chỉ lớn hơn b thì hiển thị The average value is greater than b
+# Nếu avg chỉ lớn hơn c thì hiển thị The average value is greater than c
+# Ví dụ:
+
+# Nếu a = 10, b = 20, c = 20 thì màn hình hiển thị The average value is greater than a 
+# Giải thích: avg = (10+20+20)/3 = 20 nên avg chỉ lớn hơn a.
+
+# Dùng input()
