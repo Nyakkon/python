7ba8afc1d1d72e301cca87d4609824fc339d11db
Nyakkon <Kyuubi.yae@wibu.me>
2023-07-14 20:40:38 +0700

    Update

---
+++ baitapxungdot.py
@@ -0,0 +1,7 @@
+#Viết một chươg trình yêu cầu người dùng nhập vào cân nặng kg (sử dụng float) và chiều cao (m), sau đó tính chỉ số BMI và in ra thông báo tương ứng
+# bmi < 18.5 (Dưới chuẩn)
+# 18.5 nhỏ hơn hoặc bằng bmi nhỏ hơn 25 (Bình thưởng)
+# 25 nhỏ hơn hoặc bằng bmi nhỏ hơn 30 (Thừa cân)
+# Trường hợp còn lại = Béo phì
+
+###
