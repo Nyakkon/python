cfab966e19cfad4b0b1632ae6a6b8934af48363c
Nyakkon <Kyuubi.yae@wibu.me>
2023-07-14 21:29:45 +0700

    Update

---
+++ ontap.py
@@ -0,0 +1,5 @@
+#Bài 1: Viết một chương trình yêu cầu người dùng nhập tên của họ, sau đó in ra lời chào theo định dạng "xin chào, [Tên người dùng]"
+
+#Bài 2: Nhập tên người dùng, tối đa 5 người dùng, sử dụng vòng lặp Zip để chào 5 người kèm độ tuổi và phải có input()
+
+#Bài 3: Sử dụng range(start, stop, step) và phải sử dụng input
