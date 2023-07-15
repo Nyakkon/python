f7238461d38fbae90b8a4d62cb48f36990965ebd
Nyakkon <Kyuubi.yae@wibu.me>
2023-07-15 21:36:28 +0700

    Update

--- Vòng lặp.py
+++ Vòng lặp.py
@@ -8,3 +8,25 @@ while i <= number:
     i += 1
 
 print("Tổng các số từ 1 đến", number, "là:", total)
+
+
+#bài 2:
+
+num = 2
+while num <= 100
+    check = True
+
+    if num == 2
+        check = True
+    else:
+        test = 2
+        while test * test <= num:
+            if num % test == 0:
+                check = False
+            break
+        test += 1
+
+if check:
+    print(num)
+
+num += 1
