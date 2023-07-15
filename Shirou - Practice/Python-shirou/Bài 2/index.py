52a102a99732727c63e09863a625a3c19ddbd3ae
Nyakkon <Kyuubi.yae@wibu.me>
2023-07-15 20:55:33 +0700

    Bài tap

---
+++ Shirou - Practice/Python-shirou/Bài 2/index.py
@@ -0,0 +1,10 @@
+banbenhucc = ["Đạt Gay", "Wibu Lỏd", "Tứn Mất Nhung", "Dơ Quá Ba Ơi", "Tứn Hok Gay"]
+for tundeptrai in banbenhucc:
+    print("Hi " + tundeptrai)
+
+
+numbers = [1, 2, 3, 4, 5]
+sum = 0
+for num in numbers:
+    sum+=num
+    print("Tổng các số trong danh sách là: ", sum)
